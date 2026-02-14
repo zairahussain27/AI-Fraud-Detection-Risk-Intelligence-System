d2345963fc685f0bbe13f4bbf9dcc41afac105ce
Zaira <zairahussain27@gmail.com>
2026-02-14 22:56:03 +0530

    Added initial SQL fraud analysis queries

---
+++ sql/fraud_analysis_queries.sql
@@ -0,0 +1,32 @@
+--CREATING DATABASE
+CREATE DATABASE fraud_db;
+
+--USING DATABASE
+USE fraud_db;
+
+--KNOWING THE TRASACTION TABLE
+EXEC sp_help transactions_raw;
+
+--TOP 10 RECORDS FROM TRANSACTION
+SELECT TOP 10 * FROM transactions_raw;
+
+--TOTAL TRANSACTIONS
+SELECT COUNT(*) AS Total_Transaction FROM transactions_raw;
+
+--TOTAL NUMBER OF FRAUD TRANSACTION
+SELECT COUNT(is_fraud) AS No_of_fraud FROM transactions_raw
+WHERE is_fraud = 1;
+
+--FRAUD PERCENTAGE
+SELECT (CAST(SUM(CASE WHEN is_fraud=1 THEN 1 ELSE 0 END )AS FLOAT)/COUNT(*))*100 AS Fraud_Percentage
+FROM transactions_raw;
+
+--FRAUD BY HOUR
+SELECT hour, COUNT(*) AS fraud_count FROM transactions_raw
+WHERE is_fraud=1
+GROUP BY hour ORDER BY fraud_count DESC;
+
+--HIGH AMOUNT FRAUD
+SELECT TOP 20 amt FROM transactions_raw
+WHERE is_fraud=1
+ORDER BY amt;
