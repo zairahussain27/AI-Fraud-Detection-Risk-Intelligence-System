fea3b4d42f6831310ba4f84f55ae434a93afb79b
Zaira <zairahussain27@gmail.com>
2026-02-15 17:56:46 +0530

    Addded buisness analysis queries for fraud pattern detection

--- sql/fraud_analysis_queries.sql
+++ sql/fraud_analysis_queries.sql
@@ -30,3 +30,26 @@ GROUP BY hour ORDER BY fraud_count DESC;
 SELECT TOP 20 amt FROM transactions_raw
 WHERE is_fraud=1
 ORDER BY amt;
+
+--AVG FRAUD AMOUNT 
+SELECT AVG(amt) AS Average_Fraud_Amount FROM transactions_raw
+WHERE is_fraud=1;
+
+--REPEATED CUSTOMER FRAUD
+SELECT firstname,lastname,COUNT(*) AS fraud_count FROM transactions_raw
+WHERE is_fraud=1
+GROUP BY cc_num,firstname,lastname
+ORDER BY fraud_count DESC;
+
+--REPEATED LOCATION FRAUD
+SELECT state, COUNT(*) fraud_cases
+FROM transactions_raw
+WHERE is_fraud = 1
+GROUP BY state
+ORDER BY fraud_cases DESC;
+
+--REPEATED JOB FRAUD
+SELECT job,COUNT(*) AS fraud_count FROM transactions_raw
+WHERE is_fraud=1
+GROUP BY job
+ORDER BY fraud_count DESC;
